8f1b042b5ade641204cd4e778fb9137e276f83b6
Gard Mikael <gardmikael@gmail.com>
2025-02-21 11:55:48 +0100

    Update VideoBackground.tsx

--- src/components/VideoBackground.tsx
+++ src/components/VideoBackground.tsx
@@ -9,9 +9,7 @@ export function VideoBackground() {
 			loop: 1,
 			fs: 0,
 			iv_load_policy: 3,
-			showInfo: 0,
 			mute: 1,
-			modestbranding: 1,
 			playlist: process.env.NEXT_PUBLIC_VIDEO_ID,
 		},
 	}
