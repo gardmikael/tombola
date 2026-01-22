e1a497a600a03dd29731c11f397ac6467e54844a
Gard Mikael <gardmikael@gmail.com>
2026-01-22 16:21:11 +0100

    Update Stepper.tsx

--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -2,11 +2,12 @@ import { useApp } from "@/context/AppContext"
 import { Tombola } from "./Tombola"
 import { Users } from "./Users"
 import { Transactions } from "./Transactions"
+import { ReactElement } from "react"
 
 export function Stepper() {
 	const { step, setStep } = useApp()
 
-	const Stepper: Record<string, JSX.Element> = {
+	const Stepper: Record<string, ReactElement> = {
 		users: <Users />,
 		tombola: <Tombola />,
 		transactions: <Transactions onClose={() => setStep("users")} />,
